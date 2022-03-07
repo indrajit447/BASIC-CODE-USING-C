56f8908e7add06b557b48138470f6c1dc780eda0
indrajit447 <naskarindrajit557@gmail.com>
2022-03-07 12:12:30 +0530

    changes in generateRandomnumber code
    Signed-off-by: indrajit447 <naskarindrajit557@gmail.com>

--- Rock_Paper_Scissors.c
+++ Rock_Paper_Scissors.c
@@ -4,7 +4,7 @@
 int generateRandomNumber(int n){
     srand(time(NULL));//srand takes seed as an input and is defined inside stdlib.h
     return rand()%n;
-    // printf("The random number between 0 to 5 is %d\n",generteRandomNumber(2));
+    /*printf("The random number between 0 to 5 is %d\n",generteRandomNumber(2));*/
 }
 int greater(char char1,char char2){
   //For rock paper scissors - Returns 1 if c1>c2 and 0 otherwise.If c1==c2 it will return -1
